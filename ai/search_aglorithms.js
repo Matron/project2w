3536155f30506b674fbd2bf2b72430462edbbbb4
Juan M Rondan <jrondan002@nam.ad.pwcinternal.com>
2018-01-12 18:07:28 -0300

    graph algorithm

--- ai/search_aglorithms.js
+++ ai/search_aglorithms.js
@@ -1,4 +1,18 @@
 //Depth First Search (DFS)
 GraphSearchDFS = {
-    
+    graph: null,
+    visited: null,
+    route: null,
+    source: -1,
+    target: -1,
+    found: false,
+
+    search: function() {
+        
+        return this.found;
+    },
+
+    getPathToTarget: function() {
+
+    }
 }
